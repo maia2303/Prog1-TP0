6810461d1406721dadaa6e5de8d66baca2082922
Maia <bulaciomaia23@gmail.com>
2025-10-01 10:32:15 -0300

    Creando clase Estudiante

--- Trabajo0/src/App.java
+++ Trabajo0/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        Estudiante e1 = new Estudiante(); //constructor sin parámetros
+        
+        Estudiante e2 = new Estudiante("Lucas", "Chavez", 26, "Ingeniería en Sistemas", 8.0f);
+        //constructor con parámetros
+        Estudiante e3 = new Estudiante("Lucía", "Suarez", 21, "Electrónica", 9.8f);
+
+        Estudiante[] estudiantes = { e1, e2, e3 }; //array para almacenar y recorrer
+
+        for (int i = 0; i < 3; i++) {
+        System.out.println("Nombre: " + estudiantes[i].nombre + ", Promedio: " + estudiantes[i].promedio);
+        }
     }
 }
+
+
