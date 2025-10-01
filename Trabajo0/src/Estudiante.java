51e5be4e183a8979f7df5239e658bdd77a2df25d
Maia <bulaciomaia23@gmail.com>
2025-10-01 11:19:08 -0300

    Encapsulamiento clase Estudiante

--- Trabajo0/src/Estudiante.java
+++ Trabajo0/src/Estudiante.java
@@ -1,9 +1,52 @@
 public class Estudiante {
-   public String nombre; //usar mayúscula en String
-   public String apellido;
-   public int edad; 
-   public String carrera;
-   public float promedio;
+   private String nombre; //usar mayúscula en String
+   private String apellido;
+   private int edad; 
+   private String carrera;
+   private float promedio;
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        if (nombre == null || nombre.trim().isEmpty()) {
+            System.out.println(("El nombre no puede estar vacio"));
+        } else {
+         this.nombre = nombre;
+        }
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido){
+        if (apellido == null || apellido.trim().isEmpty()) {
+            System.out.println(("El apellido no puede estar vacio"));
+        } else {
+         this.apellido = apellido;
+        }
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public float getPromedio() {
+        return promedio;
+    }
+    public void setPromedio(float promedio) {
+        this.promedio = promedio;
+    }
 
     public Estudiante () { //constructor sin parámetros
         nombre = " ";
